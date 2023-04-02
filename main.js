09fbe67888fa9fbfde6af794f141420c342cda16
James Powis <jpowis@deakin.edu.au>
2023-04-02 21:22:33 +1000

    update with additional validation rules

--- main.js
+++ main.js
@@ -9,11 +9,23 @@ app.get('/add/:num1/:num2', (req, res) => {
         const number1 = parseFloat(req.params.num1);
         const number2 = parseFloat(req.params.num2);
 
+        if(number1 !== number1) {//check if number1 is valid number
+            console.log('Number 1 is not a valid number');
+            res.send('Number 1 is not a valid number');
+        }
+
+        else if (number2 !== number2) {//check if number2 is valid number
+            console.log('Number 2 is not a valid number');
+            res.send('Number 2 is not a valid number');
+        }
+
+        else {
         const result = number1 + number2; //calculate result using addition
 
         res.send(result.toString()); //send result after converting to string
 
         console.log(number1 + '+' + number2 + '=' + result); //write to console
+        }
     } catch (error) {
         res.send(error);
     }
@@ -26,11 +38,24 @@ app.get('/subtract/:num1/:num2', (req, res) => {
         const number1 = parseFloat(req.params.num1);
         const number2 = parseFloat(req.params.num2);
 
+        if(number1 !== number1) {//check if number1 is valid number
+            console.log('Number 1 is not a valid number');
+            res.send('Number 1 is not a valid number');
+        }
+
+        else if (number2 !== number2) {//check if number2 is valid number
+            console.log('Number 2 is not a valid number');
+            res.send('Number 2 is not a valid number');
+        }
+
+        else {
+
         const result = number1 - number2; //calculate result using subtraction
 
         res.send(result.toString()); //send result after converting to string
 
         console.log(number1 + '-' + number2 + '=' + result); //write to console
+        }
     } catch (error) {
         res.send(error);
     }
@@ -43,11 +68,24 @@ app.get('/multiply/:num1/:num2', (req, res) => {
         const number1 = parseFloat(req.params.num1);
         const number2 = parseFloat(req.params.num2);
 
+        if(number1 !== number1) {//check if number1 is valid number
+            console.log('Number 1 is not a valid number');
+            res.send('Number 1 is not a valid number');
+        }
+
+        else if (number2 !== number2) {//check if number2 is valid number
+            console.log('Number 2 is not a valid number');
+            res.send('Number 2 is not a valid number');
+        }
+
+        else {
+
         const result = number1 * number2; //calculate result using multiplication
 
         res.send(result.toString()); //send result after converting to string
 
         console.log(number1 + '*' + number2 + '=' + result); //write to console
+        }
     } catch (error) {
         res.send(error);
     }
@@ -60,7 +98,19 @@ app.get('/divide/:num1/:num2', (req, res) => {
         const number1 = parseFloat(req.params.num1);
         const number2 = parseFloat(req.params.num2);
         
-        if(number2 == 0){ //check if denominator is a zero
+        if(number1 !== number1) {//check if number1 is valid number
+            console.log('Number 1 is not a valid number');
+            res.send('Number 1 is not a valid number');
+        }
+
+        else if (number2 !== number2) {//check if number2 is valid number
+            console.log('Number 2 is not a valid number');
+            res.send('Number 2 is not a valid number');
+        }
+
+        else {
+
+        if(number2 == 0) { //check if denominator is a zero
             console.log('Cannot divide by zero'); //write to console
             res.send('Cannot divide by zero'); //send result as string
         }
@@ -71,6 +121,7 @@ app.get('/divide/:num1/:num2', (req, res) => {
 
         console.log(number1 + '*' + number2 + '=' + result); //write to console
         }
+    }
     } catch (error) {
         res.send(error);
     }
